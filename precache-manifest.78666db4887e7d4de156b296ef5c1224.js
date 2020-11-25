71e6790f77d3de953f33943598bb26230f28f035
pankajshah1 <pankaj.073bct550@acem.edu.np>
2020-11-25 10:29:59 +0545

    Updates

---
+++ precache-manifest.78666db4887e7d4de156b296ef5c1224.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d137a65fde5e427af06cd785b979787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3120da16258f51a5221b",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "6275f0f37d716fce2c8e",
+    "url": "/static/css/main.45eaa588.chunk.css"
+  },
+  {
+    "revision": "3120da16258f51a5221b",
+    "url": "/static/js/2.a74d9e9d.chunk.js"
+  },
+  {
+    "revision": "c3dd763024df1e349a7e70d64f41789f",
+    "url": "/static/js/2.a74d9e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6275f0f37d716fce2c8e",
+    "url": "/static/js/main.d8e2f620.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
